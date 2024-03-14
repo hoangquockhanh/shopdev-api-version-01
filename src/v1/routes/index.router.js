6ffba4ffa6823ff922e015d4435e167f184d2dab
Hoàng Quốc Khánh <hoanghq@huongvietgroup.com>
2024-03-14 15:20:47 +0700

    FEAT: add api user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+
+router.get("/api/users", (req, res, next) => {
+  res.status(200).json({
+    status: "success api",
+    message: "api ok",
+    metadata: [
+      {
+        name: "khanh",
+        age: 26,
+      },
+      {
+        name: "ronaldo",
+        age: 39,
+      },
+      {
+        name: "messi",
+        age: 37,
+      },
+    ],
+  });
+});
 
 module.exports = router;
